d406c4f69935df01ac28dfe66a3ae93194bf3f9f
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-09-16 14:29:30 +0530

    Updates

---
+++ precache-manifest.d5cc24e9319a9ef1a883c318fc0faa14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca39fe9c968e0b95b8fa8107e2e72a4",
+    "url": "/Google-Keep-App-Clone-React/index.html"
+  },
+  {
+    "revision": "0e63d6a2c51341c68081",
+    "url": "/Google-Keep-App-Clone-React/static/css/main.26abda62.chunk.css"
+  },
+  {
+    "revision": "6ddb4d27c7c4369e5169",
+    "url": "/Google-Keep-App-Clone-React/static/js/2.c6e2a7cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Google-Keep-App-Clone-React/static/js/2.c6e2a7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e63d6a2c51341c68081",
+    "url": "/Google-Keep-App-Clone-React/static/js/main.b0a6a985.chunk.js"
+  },
+  {
+    "revision": "aa5a8a56d82234361a24",
+    "url": "/Google-Keep-App-Clone-React/static/js/runtime-main.c726bb73.js"
+  },
+  {
+    "revision": "c6b0d8821ef012073b970069274491ba",
+    "url": "/Google-Keep-App-Clone-React/static/media/logo.c6b0d882.png"
+  }
+]);
